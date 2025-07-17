b3a536cbf4bb287b41d85e6c5bca10c28c6faa19
GeorgePrempeh <gprempeh21@hotmail.com>
2025-07-17 14:34:29 +0100

    Initial commit: README generator project with Python script, requirements, and documentation

---
+++ README.md
@@ -0,0 +1,16 @@
+# my second read me project
+
+## Description
+give it all inof on read me
+
+## Installation
+create and build 
+
+## Usage
+for all
+
+## License
+This project is licensed under the MIT License.
+
+## Author
+gp group
---
+++ readme_generator.py
@@ -0,0 +1,48 @@
+from InquirerPy import prompt
+from rich.console import Console
+
+console = Console()
+
+license_options = [
+    "MIT License",
+    "Apache License 2.0",
+    "GNU General Public License (GPL v3)",
+    "GNU Lesser General Public License (LGPL v3)",
+    "Mozilla Public License 2.0 (MPL 2.0)",
+    "Creative Commons Licenses (CC0, CC BY, etc.)",
+    "Unlicense"
+]
+
+questions = [
+    {"type": "input", "name": "title", "message": "Project Title:"},
+    {"type": "input", "name": "description", "message": "Project Description:"},
+    {"type": "input", "name": "installation", "message": "Installation Instructions:"},
+    {"type": "input", "name": "usage", "message": "Usage Instructions:"},
+    {"type": "list", "name": "license", "message": "Choose a license:", "choices": license_options},
+    {"type": "input", "name": "author", "message": "Author Contact Information:"}
+]
+
+answers = prompt(questions)
+
+readme_content = f"""# {answers['title']}
+
+## Description
+{answers['description']}
+
+## Installation
+{answers['installation']}
+
+## Usage
+{answers['usage']}
+
+## License
+This project is licensed under the {answers['license']}.
+
+## Author
+{answers['author']}
+"""
+
+with open("README.md", "w") as f:
+    f.write(readme_content)
+
+console.print("[bold green]README.md has been successfully created![/bold green]")
